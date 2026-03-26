6171a1d327f8e85d345f302497de4f94b3bdbc7f
growtejas <tejaspatiloppo@gmail.com>
2026-03-26 15:07:03 +0530

    logic practice

--- tracktask/src/App.jsx
+++ tracktask/src/App.jsx
@@ -6,8 +6,31 @@ function App() {
   const [text, setText] = useState("");
   const [tasks, setTasks] = useState([]);
   const [completed, setCompleted] = useState(true)
+  const [onoff, setOnoff] = useState(false);
+  const [showHide, setShowHide] = useState(false);
+  const [txt, setTxt] = useState("")
 
+  const [t, setT] = useState("");
+  const [list, setList] = useState([]);
 
+  
+
+  function handleChange(e) {
+    setTxt(e.target.value);
+  }
+
+  function liveChange(e) {
+    setT(e.target.value);
+  }
+  function add(){
+    if(t.trim() !== ""){
+      setList(prevlist => [
+        ...prevlist,
+        {text: t}
+      ]);
+      setT("")
+    }
+  }
   function addTask(){
     if(text.trim() !== ""){
       setTasks(prevTasks =>[
@@ -32,7 +55,17 @@ function App() {
     : task
     )
     );
+    
   }
+  function change(){
+    setOnoff(!onoff);
+    }
+
+     function showChange(){
+    setShowHide(!showHide);
+    }
+
+
   return (
     <div>
       <input type="text" value={text} placeholder='Add Task..' onChange={(e) => setText(e.target.value)} />
@@ -51,6 +84,27 @@ function App() {
             </li>
         ))}
       </ul>
+      <button onClick={change}>Button</button>
+        {onoff ? <h3>On</h3> :<h3>Off</h3>}
+
+        <button onClick={showChange} >{showHide ? "Hide" : "Show"}</button>
+        {showHide && <p>This is secret text</p>}
+
+        <input type="text" value={txt} onChange={handleChange} />
+        <h3>{txt.length}</h3>
+
+        <div>
+        <input type="text" value={t} onChange={liveChange} />
+        <button onClick={add}>Add</button>
+
+        <ul> 
+          {list.map((char, index) => (  
+            <li key = {index}>
+              {char.text}
+            </li>         
+          ))}
+        </ul>
+        </div>
     </div>
   )
 }
